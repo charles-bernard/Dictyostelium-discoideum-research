c261e8c30cf65076e11b2da61b79f4e2d6895c8c
charles <charles.bernard@cri-paris.org>
2016-11-18 13:33:48 +0100

    renamed the folder

--- BinocularSwitches/SWITCH_SCRIPTS/WHITE1/WHITE1.ino
+++
@@ -1,9 +0,0 @@
-const int white1 = 3;
-
-void setup() {
-  pinMode(white1, OUTPUT);
-}
-
-void loop() {
-  analogWrite(white1, 20);
-}
--- BinocularSwitches/SWITCH_SCRIPTS/WHITE2/WHITE2.ino
+++
@@ -1,9 +0,0 @@
-const int white2 = 11;
-
-void setup() {
-  pinMode(white2, OUTPUT);
-}
-
-void loop() {
-  analogWrite(white2, 20);
-}
